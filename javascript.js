96e1b11719b72db2a26d46b0d353081c994d6838
James Hunt <37090809+James-J-Hunt@users.noreply.github.com>
2019-04-28 18:12:01 +1200

    Making the Code Concise
    Still using the same basis code but removed the extra things that I don't think will be needed.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-//Version 1
+//Version 2
 function init() {
   //Find our div containers in the DOM
   var dataContainerOrientation = document.getElementById('dataContainerOrientation');
@@ -14,19 +14,5 @@ function init() {
       if(alpha!=null || beta!=null || gamma!=null) 
         dataContainerOrientation.innerHTML = 'alpha: ' + alpha + '<br/>beta: ' + beta + '<br />gamma: ' + gamma;
     }, false);
-  }
-  // Check for support for DeviceMotion events
-  if(window.DeviceMotionEvent) {
-    window.addEventListener('devicemotion', function(event) {
-      var x = event.accelerationIncludingGravity.x;
-      var y = event.accelerationIncludingGravity.y;
-      var z = event.accelerationIncludingGravity.z;
-      var r = event.rotationRate;
-      var html = 'Acceleration:<br />';
-      html += 'x: ' + x +'<br />y: ' + y + '<br/>z: ' + z+ '<br />';
-      html += 'Rotation rate:<br />';
-      if(r!=null) html += 'alpha: ' + r.alpha +'<br />beta: ' + r.beta + '<br/>gamma: ' + r.gamma + '<br />';
-        dataContainerMotion.innerHTML = html;                  
-    });
   }
 }
