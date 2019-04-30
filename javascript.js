62e49e5de5d6bc29638276adb4049e2162f0c4c7
James Hunt <37090809+James-J-Hunt@users.noreply.github.com>
2019-05-01 01:35:08 +1200

    Another Shot

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-// Version 26
+// Version 27
 
 // Compass Code and alpha data etc inspired and adapted from HTML5 for the Mobile Web: Device Orientation Events
 // https://mobiforge.com/design-development/html5-mobile-web-device-orientation-events
@@ -122,11 +122,11 @@ function compass (latD, longD) {
 
       colourChanger = 200 - (northDegree*0.6);
 
-      if (colourChange < 5) {
+      if (colourChange <= 5) {
         backColour.style.backgroundColor = 'rgb(66, 244, 101)';
       }
-      else {
-        backColour.style.backgroundColor = 'rgb(' + colourChanger + ', 60 , 60)';
+      else if (colourChange > 5) {
+        backColour.style.backgroundColor = 'rgb(60, 60 , 60)';
       }
 
 
