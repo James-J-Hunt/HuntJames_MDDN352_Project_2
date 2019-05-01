9ed9162d283e9adc309474ee156d9d01609f9e7b
James Hunt <37090809+James-J-Hunt@users.noreply.github.com>
2019-05-01 13:57:04 +1200

    CSS Finished

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-// Version 47
+// Version 48
 
 // Compass Code and alpha data etc inspired and adapted from HTML5 for the Mobile Web: Device Orientation Events
 // https://mobiforge.com/design-development/html5-mobile-web-device-orientation-events
